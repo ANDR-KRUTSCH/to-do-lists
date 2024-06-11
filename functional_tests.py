df6b287f0ba985128b966e61ddf68123a7cafe61
Andrew Krutsch <andr.krutsch@gmail.com>
2024-06-11 17:55:32 +0500

    Now first FT uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''new visitor's test'''
 
-assert 'Congratulations' in browser.title
+    def setUp(self):
+        '''installing'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        '''uninstalling'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''can start list and retrieve it later'''
+
+        # Andrew have known about a new cool web-app with To-Do Lists. He disided to check it's home page. 
+        self.browser.get('http://localhost:8000')
+
+        # He sees that a title of the home page says about To-Do Lists 
+        self.assertIn('To-Do', self.browser.title)
+        self.fail()
+
+        # This app offer him to enter a list's item
+
+        # He entering to text-field: "I have to learn TDD"
+
+        # When he presses Enter keyboard button, the page updates and then list contains: "I have to learn TDD". 
+
+        # The page still offering to enter a new list's item.
+        # He entering: "I have to learn FastAPI".
+
+        # Then the page updates again and list contains two items now.
+
+        # It's interesting to him will this site save his To-Do list. Then he sees, that the site have generated an uniq URL to him and shows a message with text explanasions about it.
+
+        # He visites this URL and his To-Do list is still there.
+
+        # He goes to sleep.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
