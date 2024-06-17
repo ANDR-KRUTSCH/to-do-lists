94d725de38d3a9459b6d97abe059749bb36e2388
Andrew Krutsch <andr.krutsch@gmail.com>
2024-06-17 16:44:45 +0500

    New URL + view for adding items to existing lists. FT works :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('^$', views.home_page, name='home'),
     re_path('^lists/new$', views.new_list, name='new_list'),
-    re_path('^lists/the_uniqest_address/$', views.view_list, name='view_list'),
+    re_path('^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path('^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
