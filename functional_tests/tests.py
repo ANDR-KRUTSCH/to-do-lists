47df082a4543c8ce11c3b2f73bac68552bd82edd
Andrew Krutsch <andr.krutsch@gmail.com>
2024-06-17 08:35:11 +0500

    Test for testing multiple users can start list was added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,25 +1,35 @@
-import unittest
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
 class NewVisitorTest(LiveServerTestCase):
     '''new visitor's test'''
 
+    MAX_WAIT = 10
+
     def setUp(self):
-        '''installing'''
+        '''installing'''        
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         '''uninstalling'''
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''can start list and retrieve it later'''
@@ -41,8 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When he presses Enter keyboard button, the page updates and then list contains: "I have to learn TDD".
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table(row_text='1: I have to learn TDD')
+        self.wait_for_row_in_list_table(row_text='1: I have to learn TDD')
 
         # The page still offering to enter a new list's item.
         # He entering: "I have to learn FastAPI".
@@ -50,16 +59,53 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('I have to learn FastAPI')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Then the page updates again and list contains two items now.
-        self.check_for_row_in_list_table(row_text='1: I have to learn TDD')
-        self.check_for_row_in_list_table(row_text='2: I have to learn FastAPI')
-
-        self.fail('Finish test!')
-
-        # It's interesting to him will this site save his To-Do list. Then he sees, that the site have generated an uniq URL to him and shows a message with text explanasions about it.
-
-        # He visites this URL and his To-Do list is still there.
+        self.wait_for_row_in_list_table(row_text='1: I have to learn TDD')
+        self.wait_for_row_in_list_table(row_text='2: I have to learn FastAPI')
 
         # He goes to sleep.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        '''Test: multiple users can start lists at different urls'''
+        
+        # Andrew starts the new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('I have to learn TDD')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: I have to learn TDD')
+
+        # He sees that his list has a uniq URL
+        andrew_list_url = self.browser.current_url
+        self.assertRegex(andrew_list_url, '/lists/.+')
+
+        # The new user, Leon, visits the web-site now
+
+        ## We use the new browser session to delete all info from Andrew (cookie, etc.)
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Leon visits the home page. There are no any Andrew'a list signs
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('I have to learn TDD', page_text)
+        self.assertNotIn('I have to learn FastAPI', page_text)
+
+        # Leon starts the new list by inputing a new item. It's less interesting then Andrew's list
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('I have to find a well paid job')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: I have to find a well paid job')
+
+        # Leon gets the uniq URL
+        leon_list_url = self.browser.current_url
+        self.assertRegex(leon_list_url, '/lists/.+')
+        self.assertNotEqual(leon_list_url, andrew_list_url)
+
+        # One more time: there are no any Andrew's list signs
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('I have to learn TDD', page_text)
+        self.assertIn('I have to find a well paid job', page_text)
+
+        # They both go to sleep
