5bb28dade7a1bb8f7954a283128ba9d22fbce1af
Andrew Krutsch <andr.krutsch@gmail.com>
2024-06-20 11:30:15 +0500

    Used Bootstrap for layout improving

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     '''new visitor's test'''
 
     MAX_WAIT = 10
