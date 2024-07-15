a2cd519cd83e4db0f76944bb3f130e2bbe6f5730
Andrew Krutsch <andr.krutsch@gmail.com>
2024-07-15 06:48:43 +0500

    @wait decorator was added

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,8 @@ from selenium.common.exceptions import WebDriverException
 import os
 import time
 
+MAX_WAIT = 10
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     MAX_WAIT = 10
@@ -20,6 +22,18 @@ class FunctionalTest(StaticLiveServerTestCase):
         '''uninstalling'''
         self.browser.quit()
 
+    def wait(fn):
+        def modified_dn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_dn
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -33,25 +47,21 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > self.MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         return self.browser.find_element(By.CSS_SELECTOR, 'input[name="text"]')
     
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(lambda: self.browser.find_element(By.ID, 'id_logout'))
+        self.browser.find_element(By.ID, 'id_logout')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for(lambda: self.browser.find_element(By.NAME, 'email'))
+        self.browser.find_element(By.NAME, 'email')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertNotIn(email, navbar.text)
